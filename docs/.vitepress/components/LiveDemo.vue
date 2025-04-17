adc8fb2b6af491c0e360cfb4f936fa21e85086eb
adrianmiu <florin_miu@yahoo.com>
2025-04-17 15:32:32 +0300

    Moved loading of Tailwind from the LiveDemo component to the Vitepress app

--- docs/.vitepress/components/LiveDemo.vue
+++ docs/.vitepress/components/LiveDemo.vue
@@ -31,11 +31,6 @@ onMounted(() => {
   app.use(createFormKit({}))
   usePrimeVuePreset()
   app.mount(el.value)
-  const script = document.createElement('script')
-  script.src = 'https://cdn.tailwindcss.com/' // or script.textContent = 'console.log("inline script")';
-  script.type = 'text/javascript'
-  script.defer = true // optional: defer execution
-  document.head.appendChild(script) // or document.body.appendChild(script)
 })
 </script>
 
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,5 +2,12 @@ import DefaultTheme from 'vitepress/theme'
 
 /** @type {import('vitepress').Theme} */
 export default {
-  ...DefaultTheme
+  ...DefaultTheme,
+  enhanceApp(app) {
+    const script = document.createElement('script')
+    script.src = 'https://cdn.tailwindcss.com/' // or script.textContent = 'console.log("inline script")';
+    script.type = 'text/javascript'
+    script.defer = true // optional: defer execution
+    document.head.appendChild(script) // or document.body.appendChild(script)
+  }
 }
