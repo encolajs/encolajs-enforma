b4848aa99817823ac8e9fbd74078f7378ec4fda4
adrianmiu <florin_miu@yahoo.com>
2025-04-17 09:41:33 +0300

    Fixed exports file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
+// Core setup
+export { default as createEnforma } from './createFormKit'
+export { setGlobalConfig, getGlobalConfig } from './utils/useConfig'
+
 // Core composables
 export { useValidation } from './utils/useValidation'
 export { useField } from './headless/useField'
 export { useRepeatable } from './headless/useRepeatable'
 export { useForm } from './headless/useForm'
+export { useConfig } from './utils/useConfig'
+export { useFormConfig } from './utils/useFormConfig'
+export { useTranslation } from './utils/useTranslation'
+export { useDynamicProps } from './utils/useDynamicProps'
+
+// Utilities
+export { mergeConfigs, deepMerge } from './utils/configUtils'
+export { default as applyTransformers } from './utils/applyTransformers'
+export { debounce } from './utils/debounce'
+export * from './utils/helpers'
+export { 
+  createFormEmitter, 
+  globalFormEmitter, 
+  type FormEvents,
+  type FormEventType 
+} from './utils/events'
+
+// Headless components
+export { default as HeadlessForm } from './headless/HeadlessForm'
+export { default as HeadlessField } from './headless/HeadlessField'
+export { default as HeadlessRepeatable } from './headless/HeadlessRepeatable'
+
+// Presets will be imported directly by the users
+// export { default as defaultPreset } from './presets/default'
+// export { default as primevuePreset } from './presets/primevue'
 
 // Type exports
 export type {
@@ -13,12 +42,22 @@ export type {
   ValidationRules,
   FieldState,
   EventTrigger,
+  FieldSchema,
+  FormSectionSchema,
+  EnformaSchema
 } from './types'
 
-// Components
-export { default as HeadlessForm } from './headless/HeadlessForm'
-export { default as HeadlessField } from './headless/HeadlessField'
-export { default as HeadlessRepeatable } from './headless/HeadlessRepeatable'
+export type {
+  ConfigObject,
+  DeepPartial,
+  EnformaConfig,
+  FieldPassThroughConfig,
+  BehaviorConfig,
+  ExpressionsConfig,
+  ComponentsConfig,
+  ConfigProvider,
+  UseConfigReturn
+} from './utils/useConfig'
 
 // Re-export essential validator types
 export {
@@ -26,3 +65,9 @@ export {
   ValidatorFactory,
   Validator,
 } from '@encolajs/validator'
+
+// Constants
+export { formStateKey, formConfigKey, formContextKey } from './constants/symbols'
+export { DEFAULT_CONFIG } from './constants/defaults'
+
+export { default as createFormKit } from './createFormKit'
