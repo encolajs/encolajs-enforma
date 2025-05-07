470b96cada76aa646d522f2d8c1678b74f5579bf
adrianmiu <florin_miu@yahoo.com>
2025-05-07 21:18:50 +0300

    Fixed issue with the newsletter form

--- docs/.vitepress/components/MailerLiteForm.vue
+++ docs/.vitepress/components/MailerLiteForm.vue
@@ -2,7 +2,7 @@
   <div class="ml-embedded" data-form="Jc3r79"></div>
 </template>
 
-<script>
+<script setup>
 (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
   .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
   n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
