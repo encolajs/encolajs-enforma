8f92fb5349d1a0ad630e1713c254cb8d77d83d7b
adrianmiu <florin_miu@yahoo.com>
2025-06-22 09:27:10 +0300

    Removed announcement for launch

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,13 +4,11 @@ import Tab from '../components/Tab.vue'
 import TabNav from '../components/TabNav.vue'
 import LiveDemo from '../components/LiveDemo.vue'
 import AnnouncementBar from '../components/AnnouncementBar.vue'
-import Layout from './Layout.vue'
 import './custom.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
   ...DefaultTheme,
-  Layout,
   enhanceApp({ app }) {
     app.component('Tabs', Tabs)
     app.component('Tab', Tab)
